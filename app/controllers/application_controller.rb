38ea83aeb45f9ef49a5a46edd15e3f7b5e1deb59
Patrik Bona <patrik.bona@mrhead.sk>
2013-07-03 15:33:21 +0200

    Fix http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
 		def authenticate
 		  authenticate_or_request_with_http_basic do |username, password|
-		    username == env['login'] && password == env['password']
+		    username == ENV['login'] && password == ENV['password']
 		  end
 		end
 end
