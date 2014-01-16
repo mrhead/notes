cb84084f1d3e2c54931c376e9b7b4e8b0fdab262
Patrik Bóna <patrik.bona@mrhead.sk>
2014-01-16 16:55:11 +0100

    Refactor search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,4 +1,6 @@
 class Search
+  TITLE_BOOST = 5
+
   attr_reader :search_string
 
   def initialize(search_string)
@@ -6,37 +8,47 @@ class Search
   end
 
   def notes
-    unless search_string.blank?
-      found_notes = Note.where query_args 
-      sort_by_score found_notes
-    else
+    if search_string.blank?
       Note.all
+    else
+      search_and_order_notes
     end
   end
 
   private
 
-  def query_args
+  def search_and_order_notes
+    sort_by_score(found_notes)
+  end
+
+  def found_notes
+    Note.where(where_query)
+  end
+
+  def where_query
     where_string = []
     args = []
-    search_string.downcase.split.each do |word|
+    search_string_words.each do |word|
       where_string << '(lower(title) LIKE ? OR lower(text) LIKE ?)'
-      args << "%#{word}%"
-      args << "%#{word}%"
+      args << "%#{word}%" << "%#{word}%"
     end
     [where_string.join(' AND ')] + args
   end
 
-  def score(note)
-    score = 0
-    search_string.downcase.split.each do |search_word|
-      score += note.title.downcase.scan(search_word).size * 5
-      score += note.text.downcase.scan(search_word).size
-    end
-    score
+  def search_string_words
+    search_string.downcase.split
   end
 
   def sort_by_score notes
-    notes.sort! { |a,b| score(a) <=> score(b) }.reverse
+    notes.sort! { |a,b| note_score(a) <=> note_score(b) }.reverse
+  end
+
+  def note_score(note)
+    score = 0
+    search_string.downcase.split.each do |search_word|
+      score += note.title.downcase.scan(search_word).size * TITLE_BOOST
+      score += note.text.downcase.scan(search_word).size
+    end
+    score
   end
 end
